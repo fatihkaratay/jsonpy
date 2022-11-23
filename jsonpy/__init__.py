7e8e73a5f831de24cc6a3498d09a2e9886ee5ad7
Fatih Karatay <fatihkaratay@gmail.com>
2022-11-23 13:49:00 -0500

    main entry point is added.

--- jsonpy/__init__.py
+++ jsonpy/__init__.py
@@ -6,3 +6,16 @@ Supports only for Python 3.3 and higher
 
 from .exceptions import JsonSchemaException
 from .generator import CodeGenerator
+
+__all__ = ('JsonSchemaException', 'compile_func')
+
+
+def compile_func(definition):
+    """
+    Generates a validation function for validating the JSON schema bu definition
+    """
+
+    code_generator = CodeGenerator(definition)
+    local_state = {}
+    exec(code_generator.func_code, code_generator.global_state, local_state)
+    return local_state['func']
